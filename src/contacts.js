b6e039f2ffa75fb6944846e7a25b2bff6e7d1dee
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-02 02:21:56 +0530

    Update contacts.js

--- src/contacts.js
+++ src/contacts.js
@@ -17,7 +17,7 @@ const contacts = [
   },
   {
     id: 3,
-    name: "Chuck Norris",
+    name: "Chuck Nors",
     imgURL:
       "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
     phone: "+918 372 574",
